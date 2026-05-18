3129944401830130997770879eb8772d827b142e
Cuong <anhcuongnhacai@gmail.com>
2026-05-18 18:19:26 +0700

    update

--- firebase-config.js
+++ firebase-config.js
@@ -2,14 +2,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, set, get } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-database.js";
 
+// Đã thay đổi sang thông tin Firebase mới của bạn
 const firebaseConfig = {
-  apiKey: "AIzaSyAh0hpuNtXrqJZbFLH7Bs6A7vUo73sEqIU",
-  authDomain: "goldwt123.firebaseapp.com",
-  databaseURL: "https://goldwt123-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "goldwt123",
-  storageBucket: "goldwt123.appspot.com",
-  messagingSenderId: "340852437228",
-  appId: "1:340852437228:web:e64444db949896d69a35c9"
+  apiKey: "AIzaSyABaaEBwnL65YiitN5w5_BPAE4QQDfo-5s",
+  authDomain: "fishingtycoon-c2fc9.firebaseapp.com",
+  databaseURL: "https://fishingtycoon-c2fc9-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "fishingtycoon-c2fc9",
+  storageBucket: "fishingtycoon-c2fc9.appspot.com",
+  messagingSenderId: "792471669858",
+  appId: "1:792471669858:web:117186ceaa139bfd3a953b"
 };
 
 const app = initializeApp(firebaseConfig);
